62316ab192e02617350573efba91da86bf318e1f
Laura <181015028+LauraVargas22@users.noreply.github.com.>
2025-01-12 11:14:43 -0500

    feat: :sparkles: First Exercise Create a Superheroes map

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
 /* Gestión de Superhéroes con Map en JS
     By Mariana Vargas
 */
+
+//Ejercicio 1: Crear el sistema de superhéroes
+const superheroes = new Map ([
+    [1,{
+        nombre: "Iron Man",
+        universo: "Marvel",
+        poder: "Tecnología avanzada",
+        fuerza: 85
+    }],
+    [2,{
+        nombre: "Batman",
+        universo: "DC",
+        poder: "Inteligencia y estrategia",
+        fuerza: 50
+    }],
+    [3,{
+        nombre: "Wonder Woman",
+        universo: "DC",
+        poder: "Fuerza sobrehumana",
+        fuerza: 95
+    }],
+    [4,{
+        nombre: "Capitana Marvel",
+        universo: "Marvel",
+        poder: "Energía Cósmica",
+        fuerza: 98
+    }],
+    [5,{
+        nombre: "Black Widow",
+        universo: "Marvel",
+        poder: "Espionaje y Artes marciales",
+        fuerza: 55
+    }]
+]);
