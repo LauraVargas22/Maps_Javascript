ebc327ebcbcac1c10e8c2d9010ed325fbac517f5
Laura <181015028+LauraVargas22@users.noreply.github.com.>
2025-01-12 13:48:41 -0500

    feat: :sparkles: Second exercise Create a function to search superheroes by ID

--- js/app.js
+++ js/app.js
@@ -4,34 +4,52 @@
 
 //Ejercicio 1: Crear el sistema de superhéroes
 const superheroes = new Map ([
-    [1,{
+    [1, {
         nombre: "Iron Man",
         universo: "Marvel",
         poder: "Tecnología avanzada",
         fuerza: 85
     }],
-    [2,{
+    [2, {
         nombre: "Batman",
         universo: "DC",
         poder: "Inteligencia y estrategia",
         fuerza: 50
     }],
-    [3,{
+    [3, {
         nombre: "Wonder Woman",
         universo: "DC",
         poder: "Fuerza sobrehumana",
         fuerza: 95
     }],
-    [4,{
+    [4, {
         nombre: "Capitana Marvel",
         universo: "Marvel",
         poder: "Energía Cósmica",
         fuerza: 98
     }],
-    [5,{
+    [5, {
         nombre: "Black Widow",
         universo: "Marvel",
         poder: "Espionaje y Artes marciales",
         fuerza: 55
     }]
 ]);
+
+//Segundo Ejercicio: Crear función para buscar superhéroes por ID
+const buscarSuperheroePorID = (id) => {
+
+    if(superheroes.has(id)) {
+        alert(`Nombre: ${superheroes.get(id).nombre}, Universo ${superheroes.get(id).universo}, Poder ${superheroes.get(id).poder}, Fuerza ${superheroes.get(id).fuerza}}`)
+    } else {
+        alert(`El id ${id} no se encuentra`);
+    }
+}
+
+console.log(buscarSuperheroePorID(9));
+
+//Tercer Ejercicio: Crear función para buscar superhéroes por Universo
+
+const buscarPorUniverso = (universo) => {
+
+}
